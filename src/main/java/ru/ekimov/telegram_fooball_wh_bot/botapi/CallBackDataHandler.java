825933372abcd6677586a38a13237ed053d1b004
Nikolai Ekimov <64614250+ek1337@users.noreply.github.com>
2021-08-06 08:57:46 +0200

    added logger for PingTask.java

--- src/main/java/ru/ekimov/telegram_fooball_wh_bot/botapi/CallBackDataHandler.java
+++ src/main/java/ru/ekimov/telegram_fooball_wh_bot/botapi/CallBackDataHandler.java
@@ -23,14 +23,13 @@ public class CallBackDataHandler {
         long chatId = callbackQuery.getMessage().getChatId();
         long adminId = callbackQuery.getFrom().getId();
         long targetId = Long.parseLong(callbackQuery.getData());
-        String targetUser = callbackQuery.getFrom().getFirstName() + " " + callbackQuery.getFrom().getLastName();
 
         Participant p = new Participant(targetId);
 
             if (ParticipantService.adminIds.contains(adminId)
                 && participantService.userIsRegistered(p)) {
                 participantService.kickParticipant(p, p.getId());
-                replyMessage.setText("<i> Игрок " + targetUser + " был удален админом из списка на игру </i>");
+                replyMessage.setText("<i> Игрок был удален админом из списка на игру </i>");
             }
             else replyMessage.setText("<i> Только админ может кикнуть игрока. </i>");
 
--- src/main/java/ru/ekimov/telegram_fooball_wh_bot/service/PingTask.java
+++ src/main/java/ru/ekimov/telegram_fooball_wh_bot/service/PingTask.java
@@ -1,5 +1,6 @@
 package ru.ekimov.telegram_fooball_wh_bot.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Service
+@Slf4j
 public class PingTask {
     @Value("${pingtask.url}")
     private String url;
@@ -19,8 +21,10 @@ public class PingTask {
             URL url = new URL("https://www.google.com/");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
+            log.info("Ping {}, OK: response code {}", url.getHost(), connection.getResponseCode());
             connection.disconnect();
         } catch (IOException e) {
+            log.error("Ping FAILED");
             e.printStackTrace();
         }
 
